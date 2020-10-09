b75b900f81f0328af4eede9876ddb09ae0fda4ac
ulysse jobard <ulysse.jobard@telecom-paristech.fr>
2020-10-09 18:47:02 +0200

    Classification WIP

---
+++ constraint.cpp
@@ -0,0 +1,20 @@
+#ifndef CONSTRAINT
+#define CONSTRAINT
+#include "vector3.cpp"
+using namespace std;
+
+class Constraint{
+
+};
+
+
+
+
+
+
+
+
+
+
+
+#endif
---
+++ mesh.cpp
@@ -0,0 +1,15 @@
+#ifndef MESH
+#define MESH
+#include "vector3.cpp"
+using namespace std;
+
+
+class Mesh{
+public:
+  vector<Vec3f> X;
+  vector<Vec3f> P;
+};
+
+
+
+#endif
