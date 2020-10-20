9ab17074a0aa4e23b4196d61087c15d77514af43
Ulysse JOBARD <ulysse.jobard@telecom-paristech.fr>
2020-10-20 14:53:37 +0200

    constraint WIP

--- constraint.cpp
+++ constraint.cpp
@@ -11,20 +11,49 @@ public:
 class LengthConstraint : public Constraint {
 public:
 
-  Edge e;
+  Vertex * A;
+  Vertex * B;
   float initialLength;
 
   void project() {
+    float d = (A->P - B->P).length();
 
-    Vec3f dpA;
+    Vec3f dpA = -(A->w / (A->w + B->w)) * (d-initialLength) * (A->P - B->P) / d;
+    Vec3f dpB = (B->w / (A->w + B->w)) * (d-initialLength) * (A->P - B->P) / d;
 
-    // to continue
-    
+    A->P += dpA;
+    B->P += dpB;
+
     cout<<"Resolved!"<<endl;
+
+    //constructor
+    LengthConstraint(Vertex* vA, Vertex* vB){
+      A = vA;
+      B = vB;
+      initialLength = (A->X - B->X).length();
+    }
   }
 };
 
+class FixConstraint : public Constraint {
+public:
 
+  Vertex * V;
+  Vec3f initialPos;
+
+  void project() {
+    V->P = initialPos;
+
+    cout<<"Resolved!"<<endl;
+  }
+
+  //constructor
+  FixConstraint(Vertex* vertex) {
+    V = vertex;
+    V->w = 0;
+    initialPos = V->X;
+  }
+};
 
 
 
